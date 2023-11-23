d526246ad9d5171ac484dca8a58695f65ffee301
Serdsj <lifannnnof@mail.ru>
2023-11-23 10:59:35 +0300

    до интеграции своего select

---
+++ js/open-menu.js
@@ -0,0 +1,14 @@
+
+const iconMenu = document.querySelector('.menu___icon')
+
+
+if (iconMenu) {
+
+    const nav = document.querySelector('.nav')
+    
+    iconMenu.addEventListener('click', function (event) {
+        document.body.classList.toggle('lock') // блокируем скролл 
+        iconMenu.classList.toggle('active');
+        nav.classList.toggle('active')
+    })
+}
